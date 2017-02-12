b28ae0e17cd50f16f06a99025cf28fd4ca317fd9
Samuel Garcia <sgaralf@gmail.com>
2017-02-12 11:28:21 +0100

    Borrado local de ficheros y en git a ver si funcioan el gitignore

--- wp-content/cache/gantry5/g5_hydrogen/compiled/yaml/6ffd709d8bfad00842413c1f883f289a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
-    'filename' => '/var/www/clients/client1/web2/web/wp-content/themes/g5_hydrogen/custom/config/home/page/body.yaml',
-    'modified' => 1465385894,
-    'data' => [
-        'doctype' => 'html'
-    ]
-];
